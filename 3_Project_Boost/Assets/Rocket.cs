bc3ec51238949147fde86e2c4b9ee551e0589870
Philip <15992056+omgphil@users.noreply.github.com>
2020-09-20 23:34:24 -0400

    Rocket
    Added the rocked and start of the input

---
+++ 3_Project_Boost/Assets/Rocket.cs
@@ -0,0 +1,33 @@
+﻿
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    private void ProcessInput()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            print("Thrusting");
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            print("Rotation Left");
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            print("Rotation Right");
+        }
+    }
+}
