cf3a2ba7f5d8e21ca1be3adb8cebf4dafdbcf1e8
Philip <15992056+omgphil@users.noreply.github.com>
2020-09-27 22:15:48 -0400

    Added Input

---
+++ 4_Argon_Assault/Assets/MusicPlayer.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake() => DontDestroyOnLoad(this);
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke(nameof(LoadFirstLevel), 2F);
+    }
+
+    private void LoadFirstLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ 4_Argon_Assault/Assets/PlayerControl.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+// TODO: Change this to use Unity new Input System
+
+public class PlayerControl : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // OBSOLETE its built in now
+        float horizontalThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        print(horizontalThrow);
+    }
+}
