ed69ce7118fdb4f985f7aeee7498ed5551297dcb
Philip <15992056+omgphil@users.noreply.github.com>
2020-10-13 19:27:06 -0400

    Added Basic Singleton Patter

--- 4_Argon_Assault/Assets/Scripts/MusicPlayer.cs
+++ 4_Argon_Assault/Assets/Scripts/MusicPlayer.cs
@@ -2,7 +2,15 @@
 
 public class MusicPlayer : MonoBehaviour
 {
-    private void Awake() => DontDestroyOnLoad(this);
+    private void Awake()
+    {
+        if (FindObjectsOfType<MusicPlayer>().Length > 1)
+        {
+            Destroy(gameObject);
+        } else {
+            DontDestroyOnLoad(this);
+        }
+    }
 
     // Start is called before the first frame update
 
