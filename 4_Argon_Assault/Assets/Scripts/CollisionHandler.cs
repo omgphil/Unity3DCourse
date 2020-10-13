d2e8fd10ba1e5ef678258f9d60a42b86030ef3c9
Philip <15992056+omgphil@users.noreply.github.com>
2020-10-13 17:50:16 -0400

    Enable game object from code

--- 4_Argon_Assault/Assets/Scripts/CollisionHandler.cs
+++ 4_Argon_Assault/Assets/Scripts/CollisionHandler.cs
@@ -2,18 +2,31 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CollisionHandler : MonoBehaviour
 {
+    [Tooltip("In Seconds")]
+    [SerializeField] float levelLoadDelay = 1f;
+    [Tooltip("FX prefab for Player")]
+    [SerializeField] GameObject deathFX;
+
+
     // make sure when you create obstacles create an box collider on it
     private void OnTriggerEnter(Collider other)
     {
         StartDeathSequence();
+        deathFX.SetActive(true);
+        Invoke("ReloadScene", levelLoadDelay);
     }
 
     private void StartDeathSequence()
     {
-        print("Player Dying");
         SendMessage("OnPlayerDeath");
+    }
+
+    private void ReloadScene()
+    {
+        SceneManager.LoadScene(1);
     }
 }
